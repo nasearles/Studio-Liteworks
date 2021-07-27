c01c3c12f3a1d8963290d552a6dc3f5f723e72f0
nasearles <78412528+nasearles@users.noreply.github.com>
2021-07-26 22:31:18 -0700

    Add project files.

---
+++ Liteworks/LightingUnits/LightingUnit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liteworks.LightingUnits
+{
+    class LightingUnit
+    {
+        private bool _isAnimated { get; set; }
+        
+        private byte[] _colorValue;
+        private int _arraySize { get; set; }
+        private int _arrayPort { get; set; }
+        public string Name { get; set; }
+
+        public LightingUnit()
+        {
+            _colorValue = new byte[3];
+            _isAnimated = false;
+            _arraySize = 60;
+            _arrayPort = 9;
+            Name = "default";
+        }
+
+        public LightingUnit(string n, int s, int p)
+        {
+            _colorValue = new byte[3];
+            _colorValue[0] = 0;
+            _colorValue[1] = 0;
+            _colorValue[2] = 0;
+            Name = n;
+            _arraySize = s;
+            _arrayPort = p;
+        }
+
+        public void SetColor(byte red, byte green, byte blue)
+        {
+            _colorValue[0] = red;
+            _colorValue[1] = green;
+            _colorValue[2] = blue;
+            
+        }
+
+
+    }
+}
---
+++ Liteworks/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Liteworks.ViewModels;
+using Liteworks.LightingUnits;
+
+namespace Liteworks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int a { get; set; }
+        public static int selected { get; set; }
+
+        public static int[] unit { get; set; }
+
+        public string Names;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            a = 0;
+            LightingUnit bars = new LightingUnit();
+            bars.Name = "Unit 1";
+            Names = bars.Name;
+        } 
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        { 
+            
+        }
+
+        private void displayUnit(object sender, RoutedEventArgs e)
+        {
+            ((Button)sender).Content = Names;
+            if (((Button)sender).BorderBrush != Brushes.Green)
+                ((Button)sender).BorderBrush = Brushes.Green;
+
+            else
+                ((Button)sender).BorderBrush = Brushes.MidnightBlue;
+        }
+
+        private void displayUnitTwo(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowSizeClick(object send, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+        }
+
+        private void WindowMinimizeClick(object send, RoutedEventArgs e)
+        {
+            if (WindowState != WindowState.Minimized)
+                WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void SideMenuClicked(object sender, RoutedEventArgs e)
+        {
+            if (a == 0)
+            {
+                DataContext = new MenuViewModel();
+                a = 1;
+            }
+            else
+            {
+                DataContext = new MenuClosedViewModel();
+                a = 0;
+            }
+            
+
+        }
+
+        private void SideMenuUnclick(object sender, RoutedEventArgs e)
+        {
+            DataContext = new MenuClosedViewModel();
+        }
+
+        private void RowDefinition_DragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void ExpandMenu(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSelect_Click(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+                rectActual.Fill = rectSelected.Fill;
+        }
+
+        //Change color cube to correlate with the brightness slider
+        private void slider_Color_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (rectColor_Background != null)
+            {
+                double imgOpacity = slider_Color.Value;
+                imgCubeWhite.Opacity = imgOpacity;
+            }
+        }
+
+        //Get color from cube on mouse hover
+        private void imgCubeWhite_MouseMove(object sender, MouseEventArgs e)
+        {
+            select_Color(sender, e);
+        }
+
+
+        //Gets color from cursor position relative to the bitmap of the hovered image
+        private void select_Color(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                BitmapSource visual_BitmapSource = get_BitmapSource_of_Element(imgCubeWhite);
+                CroppedBitmap cb = new CroppedBitmap(visual_BitmapSource, new Int32Rect((int)Mouse.GetPosition(imgCubeWhite).X, (int)Mouse.GetPosition(imgCubeWhite).Y, 1, 1));
+                byte[] pixels = new byte[4];
+
+                try
+                {
+                    cb.CopyPixels(pixels, 4, 0);
+                }
+                catch (Exception)
+                {
+                    //error
+                }
+                rectSelected.Fill = new SolidColorBrush(Color.FromRgb(pixels[2], pixels[1], pixels[0]));
+
+
+
+            }
+            catch (Exception)
+            {
+                //not much we can do i guess
+            }
+        }
+
+       
+        public BitmapSource get_BitmapSource_of_Element(FrameworkElement element)
+        {
+            // <Check>
+            if (element == null) { return null; }
+            double dpi = 96;
+            Double width = element.ActualWidth;
+            Double height = element.ActualHeight;
+
+            // init
+
+            RenderTargetBitmap bitmap_of_Element = null;
+            if (bitmap_of_Element == null)
+            {
+                try
+                {
+                    //create an empty Bitmap of element
+                    bitmap_of_Element = new RenderTargetBitmap((int)width, (int)height, dpi, dpi, PixelFormats.Default);
+
+                    // render area into bitmap
+                    DrawingVisual visual_area = new DrawingVisual();
+                    using (DrawingContext dc = visual_area.RenderOpen())
+                    {
+                        VisualBrush visual_brush = new VisualBrush(element);
+                        dc.DrawRectangle(visual_brush, null, new Rect(new Point(), new Size(width, height)));
+                    }
+
+
+                    //render
+                    bitmap_of_Element.Render(visual_area);
+
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            return bitmap_of_Element;
+        }
+
+
+
+        //byte byte_Shade = Convert.ToByte(slider_Color.Value *255)
+        //rectColor_Background.Fill = new SolidColorBrush(Color.FromRgb(byte_shade, byte_shade, byte_shade));
+        //gradStopCenter.Color = Color.FromRgb(byte_shade, byte_shade, byte_shade); 
+    }
+}
